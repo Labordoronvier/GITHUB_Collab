152e67644e5e26563d0f23edd2ec82b408487ce4
Labordoronvier <102153713+Labordoronvier@users.noreply.github.com>
2025-08-11 14:41:46 +0800

    Add files via upload

---
+++ login.php
@@ -0,0 +1,20 @@
+<?php
+$conn = mysqli_connect "localhost", "root", "", "ITPC3_Sec3");
+if (1$conn) {
+die("Connection failed");
+}
+$username = $_POST[ 'username '];
+$password = $_POST[ 'password'];
+
+$sql - "SELECT * FROM admin WHERE username='$username' AND password='$password'";
+result = mysqli_query($conn, $sql;
+
+if mysqli_num_rows ($result) > 0) {
+    echo"success =)"; 
+    exit();
+} else {
+    echo "Login failed!";
+}
+
+mysqli_close($conn;
+?>
