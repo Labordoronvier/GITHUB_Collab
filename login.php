3be1e76612830f30f21e8aa6d97d8b59c4c67431
jeremie-cell <jeremiebalondon8@gmail.com>
2025-08-11 15:01:18 +0800

    4

--- login.php
+++ login.php
@@ -7,7 +7,7 @@ $username = $_POST[ 'username '];
 $password = $_POST[ 'password'];
 
 $sql - "SELECT * FROM admin WHERE username='$username' AND password='$password'";
-$result = mysqli_query($conn, $sql;
+$result = mysqli_query($conn, $sql);
 
 if mysqli_num_rows ($result) > 0) {
     echo"success =)"; 
