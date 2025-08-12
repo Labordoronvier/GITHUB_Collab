804c1dfa52a2e123a224d45f264605e5d5690203
Labordoronvier <102153713+Labordoronvier@users.noreply.github.com>
2025-08-12 16:53:21 +0800

    Update login.php

--- login.php
+++ login.php
@@ -3,10 +3,10 @@ $conn = mysqli_connect ("localhost", "root", "", "ITPC3_Sec3");
 if (1$conn) {
 die("Connection failed");
 }
-$username = $_POST[ 'username '];
+$Email = $_POST[ 'Email '];
 $password = $_POST[ 'password'];
 
-$sql - "SELECT * FROM admin WHERE username='$username' AND password='$password'";
+$sql - "SELECT * FROM admin WHERE username='$Email' AND password='$password'";
 $result = mysqli_query($conn, $sql);
 
 if (mysqli_num_rows ($result) > 0) {
